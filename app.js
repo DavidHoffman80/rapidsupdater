709ce59a0bcb59200979083f9234110af3aabbc3
Dave Hoffman <spaghettidave80@gmail.com>
2018-02-12 09:24:21 -0500

    Added PUG

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
+// Initialize App(express)
 var app = express();
+
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home route
+app.get('/', function(req, res){
+  res.render('layout', {
+    title: 'Home'
+  });
+});
+
 // Server started on port 3000
 app.listen(3000, function(){
   console.log('Express app listening on port 3000');
