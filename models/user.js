b5027d8e19b9f7536fc98ef1e30ba436e098a290
Dave Hoffman <spaghettidave80@gmail.com>
2018-02-19 06:32:47 -0500

    Added login functionality

---
+++ models/user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+//User schema
+const UserSchema = mongoose.Schema({
+  firstName:{
+    type: String,
+    required: true
+  },
+  lastName:{
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
+  }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 // Bring in article model
 let Article = require('../models/articles');
+// Bring in user model
+let User = require('../models/user');
 
 // GET / Home
 router.get('/', function(req, res, next){
@@ -16,6 +19,67 @@ router.get('/', function(req, res, next){
 router.get('/register', function(req, res, next){
   return res.render('register', {
     title: 'Register'
+  });
+});
+
+// POST /register
+router.post('/register', function(req, res, next){
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  req.checkBody('firstName', 'Your first name is required!').notEmpty();
+  req.checkBody('lastName', 'Your last name is required!').notEmpty();
+  req.checkBody('email', 'Your e-mail is required!').notEmpty();
+  req.checkBody('email', 'Please provide a valid e-mail!').isEmail();
+  req.checkBody('password', 'A password is required!').notEmpty();
+  req.checkBody('confirmPassword', 'Passwords do not match!').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    let registrant = {};
+    registrant.firstName = firstName;
+    registrant.lastName = lastName;
+    registrant.email = email;
+    res.render('register_error', {
+      title: 'Register',
+      registrant: registrant,
+      errors: errors
+    });
+  } else{
+    let newUser = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    });
+
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          return next(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            return next(err);
+          } else{
+            req.flash('success', 'You are now register');
+            res.redirect('/login');
+          }
+        });
+      });
+    });
+  }
+});
+
+// GET /login
+router.get('/login', function(req, res, next){
+  return res.render('login', {
+    title: 'Login'
   });
 });
 
