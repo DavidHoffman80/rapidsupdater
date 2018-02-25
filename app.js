e05459a04b6afb183435af4912d5f2afdb9c34c4
Dave Hoffman <spaghettidave80@gmail.com>
2018-02-25 15:44:42 -0500

    Listening on port 5000

--- app.js
+++ app.js
@@ -109,6 +109,6 @@ app.use(function(err, req, res, next) {
 });
 
 // Server started on port 3000
-app.listen(3000, function(){
-  console.log('Express app listening on port 3000');
+app.listen(5000, function(){
+  console.log('Express app listening on port 5000');
 });
